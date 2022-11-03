b3fc9a9b997dcb559c90508c076b73ffdf1cb9f5
AhmadKheder <ahmadkhederabdulatef@gmail.com>
2022-11-03 20:41:38 +0400

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import { makeStyles } from '@mui/styles';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.App}>
       <Header/>
+      <NavBar/>
     </div>
   );
 }
